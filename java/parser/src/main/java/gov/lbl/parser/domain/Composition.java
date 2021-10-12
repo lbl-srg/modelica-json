9f072f78e5a4afa38082b59b1d22d81fff5fc9ab
Anand Prakash <anandkp92@gmail.com>
2021-10-12 14:18:34 -0700

    refactored composition to preserve order of elements

--- java/parser/src/main/java/gov/lbl/parser/domain/Composition.java
+++ java/parser/src/main/java/gov/lbl/parser/domain/Composition.java
@@ -4,10 +4,7 @@ import java.util.Collection;
 
 public class Composition {
     private Element_list element_list;
-    private Collection<Element_list> public_element_lists;
-    private Collection<Element_list> protected_element_lists;
-    private Collection<Equation_section> equation_sections;
-    private Collection<Algorithm_section> algorithm_sections;
+    private Collection<Element_section> element_sections;
     private External_composition external_composition; 
     private Annotation annotation;
 
@@ -17,13 +14,9 @@ public class Composition {
     //     json: {}
     // }
 
-    public Composition(Element_list element_list, Collection<Element_list> public_element_lists, Collection<Element_list> protected_element_lists, Collection<Equation_section> equation_sections, 
-                        Collection<Algorithm_section> algorithm_sections, External_composition external_composition, Annotation annotation) {
+    public Composition(Element_list element_list, Collection<Element_section> element_sections, External_composition external_composition, Annotation annotation) {
         this.element_list = element_list;
-        this.public_element_lists = (public_element_lists.size() > 0) ? public_element_lists : null;
-        this.protected_element_lists = (protected_element_lists.size() > 0) ? protected_element_lists : null;
-        this.equation_sections = (equation_sections.size() > 0) ? equation_sections : null;
-        this.algorithm_sections = (algorithm_sections.size() > 0) ? algorithm_sections : null;
+        this.element_sections = (element_sections.size() > 0) ? element_sections : null;
         this.external_composition = external_composition;
         this.annotation = annotation;
     }
--- java/parser/src/main/java/gov/lbl/parser/parser/CompositionVisitor.java
+++ java/parser/src/main/java/gov/lbl/parser/parser/CompositionVisitor.java
@@ -2,12 +2,19 @@ package gov.lbl.parser.parser;
 
 import java.util.List;
 
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
 import gov.lbl.antlr4.visitor.modelicaBaseVisitor;
 import gov.lbl.antlr4.visitor.modelicaParser;
 import gov.lbl.parser.domain.Algorithm_section;
 import gov.lbl.parser.domain.Annotation;
 import gov.lbl.parser.domain.Composition;
+import gov.lbl.parser.domain.Element;
 import gov.lbl.parser.domain.Element_list;
+import gov.lbl.parser.domain.Element_section;
+import gov.lbl.parser.domain.Equation;
 import gov.lbl.parser.domain.Equation_section;
 import gov.lbl.parser.domain.External_composition;
 import gov.lbl.parser.domain.External_function_call;
@@ -19,63 +26,49 @@ import java.util.ArrayList;
 public class CompositionVisitor extends modelicaBaseVisitor<Composition> {
     @Override
     public Composition visitComposition(modelicaParser.CompositionContext ctx) {
-        List<String> element_lists_types = new ArrayList<String>();
-        element_lists_types.add("None");
-
-        for (int i=1; i<ctx.getChildCount(); i++) {
-            if (ctx.getChild(i).getClass() == modelicaParser.Element_listContext.class) {
-                String public_protected_type = ctx.getChild(i-1).getText();
-                if (public_protected_type.equals("protected")) {
-                    element_lists_types.add("protected");
-                }
-                else if (public_protected_type.equals("public")) {
-                    element_lists_types.add("public");
-                }
-            }
-        }
+        Element_list element_list = null;
+        List<Element_section> element_sections = new ArrayList<Element_section>();
 
         Element_listVisitor element_listVisitor = new Element_listVisitor();
+        ElementVisitor element_visitor = new ElementVisitor();
         Equation_sectionVisitor equation_sectionVisitor = new Equation_sectionVisitor();
+        EquationVisitor equationVisitor = new EquationVisitor();
         Algorithm_sectionVisitor algorithm_sectionVisitor = new Algorithm_sectionVisitor();
-        
-        List<Element_list> element_lists = ctx.element_list()
-            .stream()
-            .map(element_list -> element_list.accept(element_listVisitor))
-            .collect(toList()); 
-        
-        Element_list element_list = element_lists.get(0);
 
-        List<String> public_dec = ctx.PUBLIC() == null ? null : ctx.PUBLIC()
-            .stream()
-            .map(PUBLIC -> PUBLIC.getText())
-            .collect(toList());
+        List<ParseTree> children = ctx.children;
+        List<TerminalNode> tokens = null;
+        String previous_element_list_modifier = null;
 
-        List<String> protected_dec = ctx.PUBLIC() == null ? null : ctx.PROTECTED()
-            .stream()
-            .map(PROTECTED -> PROTECTED.getText())
-            .collect(toList());
-        
-        List<Element_list> public_element_lists = public_dec == null ? null : new ArrayList<Element_list>();
-        List<Element_list> protected_element_lists = protected_dec == null ? null : new ArrayList<Element_list>();
-        
-        for(int i=1; i<element_lists.size(); i++) {
-            if (element_lists_types.get(i) == "public") {
-                public_element_lists.add(element_lists.get(i));
+        for (ParseTree o : children) {            
+            if ((o instanceof modelicaParser.Element_listContext)) {
+                if (previous_element_list_modifier == null) {
+                    element_list = o.accept(element_listVisitor); 
+                } else if (previous_element_list_modifier.equals("protected")) { 
+                    Element_list protected_element_list = o.accept(element_listVisitor); 
+                    element_sections.add(new Element_section(null, protected_element_list, null, null));
+                } else if (previous_element_list_modifier.equals("public")) { 
+                    Element_list public_element_list = o.accept(element_listVisitor); 
+                    element_sections.add(new Element_section(public_element_list, null, null, null));
+                }
+                
+            } else if ((o instanceof modelicaParser.Equation_sectionContext)) {
+                Equation_section equation_section = o.accept(equation_sectionVisitor);
+                element_sections.add(new Element_section(null, null, equation_section, null));
+            } else if ((o instanceof modelicaParser.Algorithm_sectionContext)) {
+                Algorithm_section algorithm_section = o.accept(algorithm_sectionVisitor);
+                element_sections.add(new Element_section(null, null, null, algorithm_section));
+            } else if ((o instanceof modelicaParser.AnnotationContext)) {
+                //nothing here
+            } else if ((o instanceof TerminalNode)) {
+                if (o.getText().equals("protected")) {
+                    previous_element_list_modifier = "protected";
+                } else if (o.getText().equals("public")) {
+                    previous_element_list_modifier = "public";
+                }
+            } else { 
+                //nothing here
             }
-
-            if (element_lists_types.get(i) == "protected") {
-                protected_element_lists.add(element_lists.get(i));
-            }            
         }
-
-        List<Equation_section> equation_sections = ctx.equation_section() == null ? null : ctx.equation_section()
-                                                                                .stream()
-                                                                                .map(equation_section -> equation_section.accept(equation_sectionVisitor))
-                                                                                .collect(toList());;
-        List<Algorithm_section> algorithm_sections = ctx.algorithm_section() == null ? null : ctx.algorithm_section()
-                                                                                .stream()
-                                                                                .map(algorithm_section -> algorithm_section.accept(algorithm_sectionVisitor))
-                                                                                .collect(toList());;
 
         Boolean external = ctx.EXTERNAL() == null ? false : true;
         String language_specification = ctx.language_specification() == null ? "" : ctx.language_specification().getText();
@@ -106,6 +99,6 @@ public class CompositionVisitor extends modelicaBaseVisitor<Composition> {
         }
         External_composition external_composition = new External_composition(language_specification, external_function_call, external_annotation);
         
-        return new Composition(element_list, public_element_lists, protected_element_lists, equation_sections, algorithm_sections, external_composition, annotation);
+        return new Composition(element_list, element_sections, external_composition, annotation);
     }
 }
