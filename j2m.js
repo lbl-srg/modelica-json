145991c740486a7c222a46d6805d48a1b01e8348
Anand Prakash <anandkp92@gmail.com>
2021-11-05 09:42:09 -0700

    updating starting script

--- j2m.js
+++ j2m.js
@@ -2,14 +2,19 @@ const path = require('path');
 const fs = require('fs');
 const stored_definitonParser = require('./json2mo/stored_definitonParser');
 
-function convertToModeica(jsonFile) {
+function convertToModelica(jsonFile, rawJson=false) {
     var fileContent = fs.readFileSync(jsonFile, 'utf8');
     //TODO: the [0] should change with the new simplified json
-	var jsonOutput = JSON.parse(fileContent)[0];
-
-    moOutput = stored_definitonParser.parse(jsonOutput);
+    var jsonOutput;
+	if (rawJson) {
+        jsonOutput = JSON.parse(fileContent)[0];
+    } else {
+        jsonOutput = JSON.parse(fileContent);
+    }
+    moOutput = stored_definitonParser.parse(jsonOutput, rawJson);
     return moOutput;
 }
 
 // console.log(convertToModeica('raw-json/FromModelica.ExtendsClause_1.json'));
-console.log(convertToModeica('raw-json/FromModelica.Modulation.json'));
+// console.log(convertToModelica('raw-json/FromModelica.Modulation.json', rawJson=true));
+console.log(convertToModelica('json/Modulation.json', rawJson=false));
