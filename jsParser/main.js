d3e4987d4b31af6abd88f01bb12864b2cda573ca
Anand Prakash <anandkp92@gmail.com>
2022-04-21 10:23:11 -0700

    adding js visitors for stored definition

---
+++ jsParser/main.js
@@ -0,0 +1,20 @@
+// import { createRequire } from "module";
+// const require = createRequire(import.meta.url);
+
+const antlr4 = require('antlr4/index');
+const modelicaParser = require('./antlrFiles/modelicaParser');
+const modelicaLexer = require('./antlrFiles/modelicaLexer');
+const stored_definitionVisitor = require('./stored_definitionVisitor');
+const fs = require('fs')
+
+var file = '/Users/akprakash/Programming/modelica/modelica-json/test/FromModelica/Modulation.mo'
+var fileContents = fs.readFileSync(file, 'utf8');
+
+const inputStream = new antlr4.InputStream(fileContents);
+const lexer = new modelicaLexer.modelicaLexer(inputStream);
+const commonTokenStream = new antlr4.CommonTokenStream(lexer);
+const parser = new modelicaParser.modelicaParser(commonTokenStream);
+const visitor = new stored_definitionVisitor.stored_definitionVisitor();
+const parseTree = parser.stored_definition()
+visitor.visitStored_definition(parseTree);
+
