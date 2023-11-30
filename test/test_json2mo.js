b7c48aa2d74629d1e9054160f6c57383d3e53e45
Anand Prakash <anandkp92@gmail.com>
2023-11-30 09:38:21 -0800

    fix standard issues

--- test/test_json2mo.js
+++ test/test_json2mo.js
@@ -11,58 +11,55 @@ mo.afterEach(() => {
 })
 
 function getFiles (topFolder, fileType) {
-  let pattern
   // get all test modelica files to convert to json
-  pattern = path.join(__dirname, topFolder, '*.'+fileType)
+  const pattern = path.join(__dirname, topFolder, '*.' + fileType)
   return glob.sync(pattern)
 }
-  
+
 mo.describe('testing json2mo', function () {
   mo.describe('testing parsing test/reference/json folder', function () {
     mo.it('testing structure', function () {
-      fs.rmSync(path.join('test', 'reverse'), { recursive: true, force: true });
-      fs.rmSync(path.join('test', 'reverse2'), { recursive: true, force: true });
+      fs.rmSync(path.join('test', 'reverse'), { recursive: true, force: true })
+      fs.rmSync(path.join('test', 'reverse2'), { recursive: true, force: true })
 
       // convert mo to json
-      inputMoFiles = getFiles(topFolder='FromModelica', fileType='mo').splice(3)
+      const inputMoFiles = getFiles('FromModelica', 'mo').splice(3)
       pa.getJsons(inputMoFiles, 'modelica', 'json', path.join('test', 'reverse'), 'false')
 
       // convert json back to mo
-      jsonOp1Files = getFiles(topFolder=path.join('reverse', 'json', 'test', 'FromModelica'), fileType='json')
+      const jsonOp1Files = getFiles(path.join('reverse', 'json', 'test', 'FromModelica'), 'json')
       jsonOp1Files.forEach(json1FilePath => {
         json1FilePath = path.join('test', json1FilePath.split(__dirname)[1])
         pa.convertToModelica(json1FilePath, path.join('test', 'reverse'), false)
       })
-      
+
       // convert modelica back to json
-      generatedMoFiles = getFiles(topFolder=path.join('reverse', 'modelica', 'test', 'reverse', 'json', 'test', 'FromModelica'), fileType='mo')
+      const generatedMoFiles = getFiles(path.join('reverse', 'modelica', 'test', 'reverse', 'json', 'test', 'FromModelica'), 'mo')
       pa.getJsons(generatedMoFiles, 'modelica', 'json', path.join('test', 'reverse2'), 'false')
 
-      jsonOp2Files = getFiles(topFolder=path.join('reverse2', 'json', 'test', 'FromModelica'), fileType='json')
-
       jsonOp1Files.forEach(file1 => {
-        tokens = file1.split(path.sep)
-        filename = tokens[tokens.length - 1]
-        console.log("checking for ", filename)
-        
-        originalJsonFilePath = path.join(__dirname, 'reverse', 'json', 'test', 'FromModelica', filename)
-        originalJson = JSON.parse(fs.readFileSync(originalJsonFilePath), 'utf8')
+        const tokens = file1.split(path.sep)
+        const filename = tokens[tokens.length - 1]
+        console.log('checking for ', filename)
 
-        newJsonFilePath = path.join(__dirname, 'reverse2', 'json', 'test', 'reverse', 'modelica', 'test', 'reverse', 'json', 'test', 'FromModelica', filename)
-        newJson = JSON.parse(fs.readFileSync(newJsonFilePath), 'utf8')
+        const originalJsonFilePath = path.join(__dirname, 'reverse', 'json', 'test', 'FromModelica', filename)
+        const originalJson = JSON.parse(fs.readFileSync(originalJsonFilePath), 'utf8')
 
-        newJson['modelicaFile'] = originalJson['modelicaFile']
-        newJson['fullMoFilePath'] = originalJson['fullMoFilePath']
-        newJson['checksum'] = originalJson['checksum']
+        const newJsonFilePath = path.join(__dirname, 'reverse2', 'json', 'test', 'reverse', 'modelica', 'test', 'reverse', 'json', 'test', 'FromModelica', filename)
+        const newJson = JSON.parse(fs.readFileSync(newJsonFilePath), 'utf8')
+
+        newJson.modelicaFile = originalJson.modelicaFile
+        newJson.fullMoFilePath = originalJson.fullMoFilePath
+        newJson.checksum = originalJson.checksum
 
         const tempOld = JSON.stringify(originalJson).trim()
         const tempNew = JSON.stringify(newJson).trim()
-    
+
         as.deepEqual(tempNew.length, tempOld.length, 'JSON result different')
       })
 
-      fs.rmSync(path.join('test', 'reverse'), { recursive: true, force: true });
-      fs.rmSync(path.join('test', 'reverse2'), { recursive: true, force: true });
+      fs.rmSync(path.join('test', 'reverse'), { recursive: true, force: true })
+      fs.rmSync(path.join('test', 'reverse2'), { recursive: true, force: true })
     })
   })
 })
