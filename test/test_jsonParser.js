92864dc2b0afa9066cdc0becb1f526b03f11a0bc
Harold Huynh <haroldhuynh@lbl.gov>
2025-06-27 09:57:29 -0700

    added unit tests for initial = false and null

--- test/test_jsonParser.js
+++ test/test_jsonParser.js
@@ -9,28 +9,74 @@ mo.afterEach(() => {
   sinon.restore()
 })
 
-class ctxMockTrue {
-    INITIAL () {
-        return true
-    }
-    statement () {
-        return [1,2,3,4,5]
-    }
-}
-
 mo.describe('testing Algorithm_sectionVisitor.js', function () {
-    mo.it('testing initial = true', function () {
-        class statementVisitorMock {
-            visitStatement (stmt) {
-                return stmt
+    mo.describe('testing visitAlgorithm_section(ctx)', function () {
+        mo.it('testing initial = true', function () {
+            class ctxMockTrue {
+                INITIAL () {
+                    return true
+                }
+                statement () {
+                    return [1,2,3,4,5]
+                }
             }
-        }
-        sinon.stub(sv, 'StatementVisitor').returns(new statementVisitorMock)
-        const visitor = new algSV()
-        const input = new ctxMockTrue()
-        const output = visitor.visitAlgorithm_section(input)
-        const referenceOutput = new algSec.Algorithm_section(true, [1,2,3,4,5])
-        as.equal(output.initial, referenceOutput.initial, 'expected = ' + referenceOutput.initial + '; actual = ' + output.initial)
-        as.deepEqual(output.statements, referenceOutput.statements, 'expected = ' + referenceOutput.statements+ '; actual = ' + output.statements)
+            class statementVisitorMock {
+                visitStatement (stmt) {
+                    return stmt
+                }
+            }
+            sinon.stub(sv, 'StatementVisitor').returns(new statementVisitorMock)
+            const visitor = new algSV()
+            const input = new ctxMockTrue()
+            const output = visitor.visitAlgorithm_section(input)
+            const referenceOutput = [true, [1,2,3,4,5]]
+            as.equal(output.initial, referenceOutput[0], 'expected = ' + referenceOutput[0] + '; actual = ' + output.initial)
+            as.deepEqual(output.statements, referenceOutput[1], 'expected = ' + referenceOutput[1] + '; actual = ' + output.statements)
+        })
+        mo.it('testing initial = false', function () {
+            class ctxMockFalse {
+                INITIAL () {
+                    return false
+                }
+                statement () {
+                    return [1,2,3,4,5]
+                }
+            }
+            class statementVisitorMock {
+                visitStatement (stmt) {
+                    return stmt
+                }
+            }
+            sinon.stub(sv, 'StatementVisitor').returns(new statementVisitorMock)
+            const visitor = new algSV()
+            const input = new ctxMockFalse()
+            const output = visitor.visitAlgorithm_section(input)
+            const referenceOutput = [false, [1,2,3,4,5]]
+            as.equal(output.initial, referenceOutput[0], 'expected = ' + referenceOutput[0] + '; actual = ' + output.initial)
+            as.deepEqual(output.statements, referenceOutput[1], 'expected = ' + referenceOutput[1] + '; actual = ' + output.statements)
+        })
+        mo.it('testing initial = null', function () {
+                class ctxMockNull {
+                INITIAL () {
+                    return null
+                }
+                statement () {
+                    return [1,2,3,4,5]
+                }
+            }
+            class statementVisitorMock {
+                visitStatement (stmt) {
+                    return stmt
+                }
+            }
+            sinon.stub(sv, 'StatementVisitor').returns(new statementVisitorMock)
+            const visitor = new algSV()
+            const input = new ctxMockNull()
+            const output = visitor.visitAlgorithm_section(input)
+            const referenceOutput = [false, [1,2,3,4,5]]
+            as.equal(output.initial, referenceOutput[0], 'expected = ' + referenceOutput[0] + '; actual = ' + output.initial)
+            as.deepEqual(output.statements, referenceOutput[1], 'expected = ' + referenceOutput[1] + '; actual = ' + output.statements)
+        })
     })
+
 })
